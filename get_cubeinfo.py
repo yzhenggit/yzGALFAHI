eb0e8871cd6bbef41b9feaab43029b100536d4d4
yhzheng <yz2505@columbia.edu>
2019-07-21 10:38:48 -0700

    Change fits header reader origin (0 or 1) as an input

--- get_cubeinfo.py
+++ get_cubeinfo.py
@@ -5,31 +5,31 @@ import numpy as np
 
 
 # a function to read in header and generate RA, DEC and VLSR array
-def get_cubeinfo(header, returnHeader=False):
+def get_cubeinfo(header, returnHeader=False, origin=1):
     '''
     A function created to parse the RA, DEC, (and velocity) information from the 2D (3D) header
 
-    - This function has been tested with GALFA-HI/EBHIS cubes, and GALFA-HI 2D images. 
+    - This function has been tested with GALFA-HI/EBHIS cubes, and GALFA-HI 2D images.
     - 		    also been tested with LAB cubes/images that are in (glon, glat) coordinates.
     - The input header can be 2D: NAXIS=2. NAXIS1 is RA (glon), 2 is DEC (glat)
     - 			   or 3D: NAXIS=3. NAXIS1 is RA (glon), 2 is DEC (glat), 3 is Velocity
-    - Return: if GALFA-HI or EBHIS: 
+    - Return: if GALFA-HI or EBHIS:
                  	ra and dec in 2D, with shape of (dec.size, ra.size) or (NAXIS2, NAXIS1)
-    -         	 	velocity in 1D.  
+    -         	 	velocity in 1D.
                         or (ra, dec, vlsr, header array)
-    - 	      if LAB: 
+    - 	      if LAB:
 			glon, glat in 2D, with shape of (glat.size, glon.size) or (NAXIS2, NAXIS1)
-    - 			velocity in 1D. 
+    - 			velocity in 1D.
                         or (gl, gb, vlsr, header array)
     - History: updated as of 2016.10.03. Yong Zheng @ Columbia Astro.
-    '''        
+    '''
 
     #import sys
     #import astropy.wcs as wcs
     #import numpy as np
 
-  
-    hdrarrs = [] 
+
+    hdrarrs = []
     if header['NAXIS'] == 2:
         hdr2d = header.copy()
         hdrarrs.append(hdr2d)
@@ -76,7 +76,7 @@ def get_cubeinfo(header, returnHeader=False):
     n1, n2 = hdr2d['NAXIS1'], hdr2d['NAXIS2']
     ax = np.reshape(np.mgrid[0:n1:1]+1, (1, n1))  # For FITS standard, origin = 1
     ay = np.reshape(np.mgrid[0:n2:1]+1, (n2, 1))  #   then for numpy standard, origin = 0
-    coor1, coor2 = gwcsa.all_pix2world(ax, ay, 1) # coor1 = ra  or glon
+    coor1, coor2 = gwcsa.all_pix2world(ax, ay, origin) # coor1 = ra  or glon
     return_arrays.append(coor1) 		  # coor2 = dec or glat
     return_arrays.append(coor2)
 
@@ -86,13 +86,12 @@ def get_cubeinfo(header, returnHeader=False):
         n1 = hdr1d['NAXIS1']
         ax = np.mgrid[0:n1:1]+1
         # ax = np.linspace(0, n1, n1)  # nope, wrong
-        vel = gwcsb.all_pix2world(ax, 1)[0]
+        vel = gwcsb.all_pix2world(ax, origin)[0]
         if 'CUNIT1' in hdr1d.keys():
-            if hdr1d['CUNIT1'] in ['m/s', 'M/S', 'M/s', 'm/S']: 
+            if hdr1d['CUNIT1'] in ['m/s', 'M/S', 'M/s', 'm/S']:
                 vel = vel/1e3
         else: vel = vel/1e3  # default is usually in m/s
         return_arrays.append(vel)
-    
+
     if returnHeader == True: return_arrays.append(hdrarrs)
     return return_arrays
-
